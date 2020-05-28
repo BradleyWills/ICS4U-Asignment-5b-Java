d024f33abc5f573544a1492d7eb1517529773093
Bradley Wills <bradley.wills@mths.ca>
2020-05-28 01:46:05 +0000

    second commit

--- MazePathfinder.java
+++ MazePathfinder.java
@@ -3,7 +3,7 @@ import java.util.Scanner;
 /**
 * The MazePathfinder program implements an application that accepts an input of a maze and
 * proceeds to find the path through the maze. It then outputs the maze with the correct path
-* being a different symbol.
+* being a different stYmbol.
 *
 * @author  Bradley Wills
 * @version 1.0
@@ -19,14 +19,14 @@ public class MazePathfinder {
   public static void main(String[] args) {
     
     boolean finished = false;
-    int sX = 0;
-    int sY = 0;
+    int stX = 0;
+    int stY = 0;
     int mazeWidth;
-    Scanner userInput = new Scanner(System.in);
+    Scanner userInput = new Scanner(stYstem.in);
     try {
-      System.out.println("Type in the maze for a solved version.");
+      stYstem.out.println("Type in the maze for a solved version.");
       String userMaze = userInput.nextLine();
-      System.out.println("Type in the width of the maze.");
+      stYstem.out.println("Type in the width of the maze.");
       // 6
       mazeWidth = userInput.nextInt();
       String[][] grid = new String[mazeWidth + 2][(userMaze.length() / mazeWidth) + 2];
@@ -39,57 +39,56 @@ public class MazePathfinder {
         for (int x = 0; x <= mazeWidth - 1; x++) {
           grid[x + 1][y + 1] = String.valueOf(userMaze.charAt(y * mazeWidth + x));
           if (String.valueOf(userMaze.charAt(y * mazeWidth + x)).equals("S")) {
-            sX = x + 1;
-            sY = y + 1;
+            stX = x + 1;
+            stY = y + 1;
           }
         }
       }
-      if (grid[sX][sY].equals("S")) {
-      } else {
-        System.out.println("Invalid Input. Make sure you have a starting point.");
+      if ((grid[stX][stY].equals("S")) == false) {
+        stYstem.out.println("Invalid Input. Make sure you have a starting point.");
         return;
       }
       while (finished == false) {
-        if ((grid[sX][sY + 1].equals(".")) || (grid[sX][sY + 1].equals("G"))) {
-          if (grid[sX][sY + 1].equals(".")) {
-            sY += 1;
-            grid[sX][sY] = "+";
+        if ((grid[stX][stY + 1].equals(".")) || (grid[stX][stY + 1].equals("G"))) {
+          if (grid[stX][stY + 1].equals(".")) {
+            stY += 1;
+            grid[stX][stY] = "+";
           } else {
             finished = true;
           }
-        } else if ((grid[sX][sY - 1].equals(".")) || (grid[sX][sY - 1].equals("G"))) {
-          if (grid[sX][sY - 1].equals(".")) {
-            sY -= 1;
-            grid[sX][sY] = "+";
+        } else if ((grid[stX][stY - 1].equals(".")) || (grid[stX][stY - 1].equals("G"))) {
+          if (grid[stX][stY - 1].equals(".")) {
+            stY -= 1;
+            grid[stX][stY] = "+";
           } else {
             finished = true;
           }
-        } else if ((grid[sX + 1][sY].equals(".")) || (grid[sX + 1][sY].equals("G"))) {
-          if (grid[sX + 1][sY].equals(".")) {
-            sX += 1;
-            grid[sX][sY] = "+";
+        } else if ((grid[stX + 1][stY].equals(".")) || (grid[stX + 1][stY].equals("G"))) {
+          if (grid[stX + 1][stY].equals(".")) {
+            stX += 1;
+            grid[stX][stY] = "+";
           } else {
             finished = true;
           }
-        } else if ((grid[sX - 1][sY].equals(".")) || (grid[sX - 1][sY].equals("G"))) {
-          if (grid[sX - 1][sY].equals(".")) {
-            sX -= 1;
-            grid[sX][sY] = "+";
+        } else if ((grid[stX - 1][stY].equals(".")) || (grid[stX - 1][stY].equals("G"))) {
+          if (grid[stX - 1][stY].equals(".")) {
+            stX -= 1;
+            grid[stX][stY] = "+";
           } else {
             finished = true;
           }
         } else {
-          grid[sX][sY] = "/";
-          if (grid[sX][sY + 1].equals("+")) {
-            sY++;
-          } else if (grid[sX][sY - 1].equals("+")) {
-            sY--;
-          } else if (grid[sX + 1][sY].equals("+")) {
-            sX++;
-          } else if (grid[sX - 1][sY].equals("+")) {
-            sX--;
+          grid[stX][stY] = "/";
+          if (grid[stX][stY + 1].equals("+")) {
+            stY++;
+          } else if (grid[stX][stY - 1].equals("+")) {
+            stY--;
+          } else if (grid[stX + 1][stY].equals("+")) {
+            stX++;
+          } else if (grid[stX - 1][stY].equals("+")) {
+            stX--;
           } else {
-            System.out.println("Invalid Input. There is no possible path.");
+            stYstem.out.println("Invalid Input. There is no possible path.");
             return;
           }
         }
@@ -107,10 +106,10 @@ public class MazePathfinder {
         for (int a = 1; a <= mazeWidth; a++) {
           lineOfText += grid[a][b];
         }
-        System.out.println(lineOfText);
+        stYstem.out.println(lineOfText);
       }
     } catch (Exception e) {
-      System.out.println("Invalid Input.");
+      stYstem.out.println("Invalid Input.");
     }
   }
 }
